31b7c9c2a4ac1bbeb6cc099f81820a3070f2a064
Ertugrul96 <ertugrulsenbalci96@gmail.com>
2025-11-20 22:37:36 +0300

    Update users.service.ts

--- integration/hello-world/src/hello/users/users.service.ts
+++ integration/hello-world/src/hello/users/users.service.ts
@@ -1,8 +1,27 @@
-import { Injectable } from '@nestjs/common';
+
+
+import { Injectable, NotFoundException } from '@nestjs/common';
+
 
 @Injectable()
 export class UsersService {
-  findById(id: string) {
-    return { id };
-  }
+    private readonly firestoreCollection = 'users'; 
+    constructor() {
+  
+    }
+
+    async findById(id: string): Promise<any> {
+        console.log(`[Firestore Sim]: Querying ${this.firestoreCollection} collection for document ID: ${id}`);
+        
+        if (id === '404-error') {
+            throw new NotFoundException(`User with ID ${id} not found in Firestore.`);
+        }
+        return {
+            id: id,
+            username: 'Ertugrul96',
+            email: 'user@example.com',
+            createdAt: new Date().toISOString(), 
+            source: `Simulated Firestore Read from NestJS Service`
+        };
+    }
 }
